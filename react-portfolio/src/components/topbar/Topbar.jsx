9216b6353cae767c53e2f061d6b0e8abc3f6552b
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-05-14 10:58:03 -0400

    edit homepage/add functionality

--- react-portfolio/src/components/topbar/Topbar.jsx
+++ react-portfolio/src/components/topbar/Topbar.jsx
@@ -3,7 +3,15 @@ import "./topbar.scss"
 export default function Topbar(){
     return (
         <div className= "topbar">
-
+            <div className = "wrapper">
+                <div className ="left">
+                    <a href ="#intro" className ="logo">wag1</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
